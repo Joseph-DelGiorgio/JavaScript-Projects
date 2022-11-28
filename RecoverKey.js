e58f0004fe8c5bfc253eb959233cdffb158d9178
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2022-11-28 13:17:59 -0700

    Create RecoverKey.js

---
+++ RecoverKey.js
@@ -0,0 +1,59 @@
+// HashMessage.js:
+
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes } = require("ethereum-cryptography/utils");
+
+function hashMessage(message) {
+    return keccak256(utf8ToBytes(message));
+}
+
+module.exports = hashMessage;
+
+
+//signMessage.js:
+
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require('./hashMessage');
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+async function signMessage(msg) {
+    const messageHash = hashMessage(msg);
+    return secp.sign(messageHash, PRIVATE_KEY, { recovered: true });
+}
+
+module.exports = signMessage;
+
+//RecoverKey.js:
+
+const secp = require("ethereum-cryptography/secp256k1");
+const hashMessage = require("./hashMessage");
+
+async function recoverKey(message, signature, recoveryBit) {
+    const messageHash = hashMessage(message);
+    return secp.recoverPublicKey(messageHash, signature, recoveryBit);
+}
+
+module.exports = recoverKey;
+
+//Test.js:
+
+const signMessage = require('../signMessage');
+const recover = require('../recoverKey');
+const secp = require("ethereum-cryptography/secp256k1");
+const { assert } = require('chai');
+const { toHex } = require("ethereum-cryptography/utils");
+
+const PRIVATE_KEY = "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+
+describe('Recover Key', () => {
+    it('should recover the public key from a signed message', async () => {
+        const [sig, recoveryBit] = await signMessage('hello world');
+
+        const publicKey = secp.getPublicKey(PRIVATE_KEY);
+
+        const recovered = await recover('hello world', sig, recoveryBit);
+
+        assert.equal(toHex(recovered), toHex(publicKey));
+    });
+});
