741843d819b709935fdd000ee1f2a040c255b810
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-16 14:03:47 -0800

    Create Proxy.js

---
+++ Proxy.js
@@ -0,0 +1,44 @@
+//deployProxy.js
+
+const { ethers, upgrades } = require('hardhat');
+
+async function main() {
+  const VendingMachineV1 = await ethers.getContractFactory('VendingMachineV1');
+  const proxy = await upgrades.deployProxy(VendingMachineV1, [100]);
+  await proxy.deployed();
+
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    proxy.address
+  );
+
+  console.log('Proxy contract address: ' + proxy.address);
+
+  console.log('Implementation contract address: ' + implementationAddress);
+}
+
+main();
+
+
+
+
+
+//upgradeProxy.js
+
+const { ethers, upgrades } = require('hardhat');
+
+// TO DO: Place the address of your proxy here!
+const proxyAddress = '';
+
+async function main() {
+  const VendingMachineV2 = await ethers.getContractFactory('VendingMachineV2');
+  const upgraded = await upgrades.upgradeProxy(proxyAddress, VendingMachineV2);
+
+  const implementationAddress = await upgrades.erc1967.getImplementationAddress(
+    proxyAddress
+  );
+
+  console.log("The current contract owner is: " + upgraded.owner());
+  console.log('Implementation contract address: ' + implementationAddress);
+}
+
+main();
