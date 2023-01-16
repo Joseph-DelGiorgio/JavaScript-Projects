69c4b09cfe3aa2258204d3286fdc012a41d270c8
Joseph Del Giorgio <106563909+Joseph-DelGiorgio@users.noreply.github.com>
2023-01-16 14:06:59 -0800

    Update Proxy.js

--- Proxy.js
+++ Proxy.js
@@ -42,3 +42,104 @@ async function main() {
 }
 
 main();
+
+
+
+
+
+
+
+
+
+
+//VendingMachinev1.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract VendingMachineV1 is Initializable {
+  // these state variables and their values
+  // will be preserved forever, regardless of upgrading
+  uint public numSodas;
+  address public owner;
+
+  function initialize(uint _numSodas) public initializer {
+    numSodas = _numSodas;
+    owner = msg.sender;
+  }
+
+  function purchaseSoda() public payable {
+    require(msg.value >= 1000 wei, "You must pay 1000 wei for a soda!");
+    numSodas--;
+  }
+}
+
+
+
+
+
+
+
+//VendingMachinev2.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract VendingMachineV2 is Initializable {
+  // these state variables and their values
+  // will be preserved forever, regardless of upgrading
+  uint public numSodas;
+  address public owner;
+
+  function initialize(uint _numSodas) public initializer {
+    numSodas = _numSodas;
+    owner = msg.sender;
+  }
+
+  function purchaseSoda() public payable {
+    require(msg.value >= 1000 wei, "You must pay 1000 wei for a soda!");
+    numSodas--;
+    // challenge: add a mapping to keep track of user soda purchases!
+  }
+
+  function withdrawProfits() public onlyOwner {
+    require(address(this).balance > 0, "Profits must be greater than 0 in order to withdraw!");
+    (bool sent, ) = owner.call{value: address(this).balance}("");
+    require(sent, "Failed to send ether");
+  }
+
+  function setNewOwner(address _newOwner) public onlyOwner {
+    owner = _newOwner;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only owner can call this function.");
+    _;
+  }
+}
+
+
+
+//Hardhat.config.js
+
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('@openzeppelin/hardhat-upgrades');
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: process.env.ALCHEMY_GOERLI_URL,
+      accounts: [process.env.GOERLI_PRIVATE_KEY]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY
+  }
+};
